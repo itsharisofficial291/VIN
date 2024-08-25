4f1da29f8d81c60314c157307f1782eaeb7e8c50
Muhammad Haris <135629302+itsharisofficial291@users.noreply.github.com>
2024-08-25 17:54:14 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// const data = null;
+
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener('readystatechange', function () {
+//     if (this.readyState === XMLHttpRequest.DONE) {
+//         if (this.status >= 200 && this.status < 300) {
+//             // Successful response
+//             console.log('Car Details:', JSON.parse(this.responseText));
+//         } else {
+//             // Handle errors
+//             console.error('Failed to fetch car details:', this.status, this.statusText);
+//             console.error('Response:', this.responseText);
+//         }
+//     }
+// });
+
+// xhr.open('GET', 'https://vin-decoder19.p.rapidapi.com/vin_decoder_extended?vin=1FMEU75837UA82077');
+// xhr.setRequestHeader('x-rapidapi-key', 'd261c87143msh04944a6375b5214p10f6ecjsnd56d503889e1');
+// xhr.setRequestHeader('x-rapidapi-host', 'vin-decoder19.p.rapidapi.com');
+
+// xhr.send(data);
